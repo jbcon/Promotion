fa6154b3e18bd9407777a836646b65bd7df74cea
Bparker10 <parker4@rpi.edu>
2014-09-28 21:55:06 -0400

    work in day 1 scene and added music (doesn't play)

--- Promotion/src/day1.js
+++ Promotion/src/day1.js
@@ -1,5 +1,25 @@
-var Day1Layer = cc.Layer.extend({
-	
 
-	
-})
+var Day1Layer = cc.Layer.extend({
+	sprite:null,
+	ctor:function () {
+		//super init
+		this._super();
+		//get window size
+		var size = cc.winSize;
+		//maybe screen with "day 1"
+		var label = new cc.LabelTTF("Day 1", "Arial", 40);
+		label.x = size.width / 2;
+		label.y = size.width / 4;
+		this.addChild(label);
+		//init music
+		//[[OALSimpleAudio sharedInstance] playFile:@"Cannery.wav"]
+	}
+});
+
+var Day1Scene = cc.Scene.extend({
+	onEnter:function(){
+		this._super();
+		var layer = new Day1Layer();
+		this.addChild(layer);
+	}
+});
