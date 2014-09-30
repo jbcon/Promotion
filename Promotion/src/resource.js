b1995a407278c97716d1bc8dd79c0f4db6fad0d1
Bparker10 <parker4@rpi.edu>
2014-09-30 10:05:29 -0400

    changed the resource file to make it easier to see what it is. i can change it back if needed but this seems easier

--- Promotion/src/resource.js
+++ Promotion/src/resource.js
@@ -1,12 +1,9 @@
 var res = {
-    HelloWorld_png : "res/HelloWorld.png",
-    CloseNormal_png : "res/CloseNormal.png",
-    CloseSelected_png : "res/CloseSelected.png",
     day1_music : "res/Audio/Cannery.wav",
     desk : "res/images/D1_deskRough.png"
 };
 
-var g_resources = [];
-for (var i in res) {
-    g_resources.push(res[i]);
-}
+var g_resources = [
+	res.day1_music,
+	res.desk
+];
