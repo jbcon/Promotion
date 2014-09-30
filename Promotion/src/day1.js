30c603e609b8bb019b1cb11bdd9bbf45b928b86e
Bparker10 <parker4@rpi.edu>
2014-09-30 12:15:52 -0400

    added in main menu functionality (new game = day1, options = day2)

--- Promotion/src/day1.js
+++ Promotion/src/day1.js
@@ -1,9 +1,6 @@
 
-var Day1Layer = cc.Layer.extend({
-	sprite:null,
+var Day1Screen = cc.Layer.extend({
 	ctor:function () {
-		//super init
-		this._super();
 		//get window size
 		var size = cc.winSize;
 		//maybe screen with "day 1"
@@ -11,18 +8,34 @@ var Day1Layer = cc.Layer.extend({
 		label.x = size.width / 2;
 		label.y = 0;
 		this.addChild(label);
-		
+
+		label.runAction(
+            cc.spawn(
+                cc.moveBy(5.5, cc.p(0, size.height + 40))
+            )
+        );
+	}
+});
+
+
+var Day1Layer = cc.Layer.extend({
+	_desk:null,
+	ctor:function () {
+		//super init
+		this._super();
+		//get window size
+		var size = cc.winSize;
 		//music
-		
+		//cc.audioEngine.playMusic(res.Cannery)
 
 		//desk+player sprite
-		this.sprite = new cc.Sprite(res.desk);
-		this.sprite.attr({
+		this._desk = new cc.Sprite(res.desk);
+		this._desk.attr({
 			x: size.width / 2,
 			y: size.height / 2,
 			scale: 0.35
 		});
-		this.addChild(this.sprite, 0);
+		this.addChild(this._desk, 0);
 
 		//keyboard event listener
     	cc.eventManager.addListener({
@@ -38,22 +51,21 @@ var Day1Layer = cc.Layer.extend({
 	        onKeyReleased: function(keyCode, event){
 	            var key = keyCode.toString();
 	            console.log(key);	        }
-    	}, this);   
+    		}, this);   
 
-    	label.runAction(
-            cc.spawn(
-                cc.moveBy(5.5, cc.p(0, size.height + 40))
-            )
-        );
+    	
 
 		return true;
 	}
 });
-
+/*
 var Day1Scene = cc.Scene.extend({
 	onEnter:function(){
 		this._super();
-		var layer = new Day1Layer();
-		this.addChild(layer);
+		var scene = new cc.Scene();
+		scene.addChild(new Day1Layer());
+		scene.addChild(new Day1Screen());
+		cc.director.runScene(new cc.TransitionFade(1.2, scene));
 	}
 });
+*/
--- Promotion/src/resource.js
+++ Promotion/src/resource.js
@@ -1,9 +1,11 @@
 var res = {
     day1_music : "res/Audio/Cannery.wav",
-    desk : "res/images/D1_deskRough.png"
+    desk : "res/images/D1_deskRough.png",
+    menu : "res/images/menu.png"
 };
 
 var g_resources = [
 	res.day1_music,
-	res.desk
+	res.desk,
+	res.menu
 ];
