7f81b971d88775b6bc2e7d555cb6d6c338f1316c
Chi Ning Huang <huangc8@rpi.edu>
2014-10-01 02:12:09 -0400

    Night Scene: movement of character sprite sheet

--- Promotion/src/night_Animation_Layer.js
+++ Promotion/src/night_Animation_Layer.js
@@ -1,4 +1,8 @@
 var AnimationLayer = cc.Layer.extend({
+    spriteSheet: null,
+    runningAction: null,
+    sprite: null,
+    
     ctor:function () {
         this._super();
         this.init();
@@ -7,12 +11,26 @@ var AnimationLayer = cc.Layer.extend({
         this._super();
 
         //create the hero sprite
-        var spriteRunner = cc.Sprite.create(res.N_player_png);
-        spriteRunner.attr({x: 80, y: 85});
-
+        cc.spriteFrameCache.addSpriteFrames(res.N_runner_plist);
+        this.spriteSheet = cc.SpriteBatchNode.create(res.N_runner_png);
+        this.addChild(this.spriteSheet);
+        
+        
+        
+        // init runningAction
+        var animFrames = [];
+        for (var i = 1; i < 8; i++) {
+            var str = "N_Player_test_" + i + ".png";
+            var frame = cc.spriteFrameCache.getSpriteFrame(str);
+            animFrames.push(frame);
+        }
+        
         //create the move action
-        var actionTo = cc.MoveTo.create(2, cc.p(300, 85));
-        spriteRunner.runAction(cc.Sequence.create(actionTo));
-        this.addChild(spriteRunner);
+        var animation = cc.Animation.create(animFrames, 0.1);
+        this.runningAction = cc.RepeatForever.create(cc.Animate.create(animation));
+        this.sprite = cc.Sprite.create("#N_Player_test_1.png");
+        this.sprite.attr({x: 80, y: 85});
+        this.sprite.runAction(this.runningAction);
+        this.spriteSheet.addChild(this.sprite);
     }
 });
--- Promotion/src/resource.js
+++ Promotion/src/resource.js
@@ -5,7 +5,9 @@ var res = {
     
     // Night
     N_BG_png : "res/images/N_PlayBG_test.png",
-    N_Player_png : "res/images/N_Player_test.png"
+    N_Player_png : "res/images/N_Player_test.png",
+    N_runner_png : "res/images/N_running.png",
+    N_runner_plist : "res/images/N_running.plist"
 };
 
 var g_resources = [
@@ -13,5 +15,7 @@ var g_resources = [
 	res.desk,
 	res.menu,
     res.N_BG_png,
-    res.N_Player_png
+    res.N_Player_png,
+    res.N_runner_png,
+    res.N_runner_plist
 ];
