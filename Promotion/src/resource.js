a21ecf7fe3c73ac6c4b961e6746f1ebe273c94a0
John Conover <111jbcon@gmail.com>
2014-10-01 23:40:47 -0400

    Infinitely scrolling background

--- Promotion/src/resource.js
+++ Promotion/src/resource.js
@@ -2,12 +2,14 @@ var res = {
     day1_music : "res/Audio/Cannery.wav",
     desk : "res/images/D1_deskRough.png",
     day2player_png: "res/images/day2player.png",
-    menu : "res/images/menu.png"
+    menu : "res/images/menu.png",
+    day2bg_png: "res/images/day2bg.png"
 };
 
 var g_resources = [
 	res.day1_music,
 	res.desk,
 	res.menu,
-	res.day2player_png
+	res.day2player_png,
+	res.day2bg_png
 ];
