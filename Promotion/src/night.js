edc1e56de46d917e918b6503588a2964494032f8
Chi Ning Huang <huangc8@rpi.edu>
2014-10-08 12:40:02 -0400

    Night + day3 update

--- Promotion/src/night.js
+++ Promotion/src/night.js
@@ -57,14 +57,21 @@ var NightScene = cc.Scene.extend({
         this.shapesToRemove = [];
         
         this.timer +=1;
-        if(this.timer == 100){
+        if(this.timer == 10000){
             this.gameOver();
         }
     },
     
     gameOver:function(){
         cc.log("==game over");
-        g_scene = 4;
+        var sL = this.getChildByTag(TagOfLayer.Status);
+        var ctmp = sL.checks;
+        g_score += ctmp*100;
+        if(g_nightState == 0){
+            g_scene = 2;
+        }else{
+            g_scene = 4;
+        }
         var scene = new TransitionScene();
         cc.director.runScene(new cc.TransitionFade(1.2, scene));
     }
